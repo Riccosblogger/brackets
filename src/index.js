5a846030b9aedfcbd07e7c65c23ad728f74d3735
Riccosblogger <riccosblogger@gmail.com>
2020-03-07 12:19:22 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str = str.split('');
+
+  if (str.length%2!=0) return false; // количество скобок должно быть чётным
+  
+  // ищем рядом стоящие парные скобки и удаляем их из массива
+  
+  var old_len;
+  
+  while (str.length>0)
+  {
+  old_len = str.length;
+  
+  for(let i=0; i<str.length; i++)
+  for (let j=0; j<bracketsConfig.length; j++)
+  if (str[i]==bracketsConfig[j][0] && str[i+1]==bracketsConfig[j][1])
+  str.splice(i,2);
+  
+  if (old_len == str.length) return false;
+  
+  }
+  
+  return true;
+  
+  };
